61716c7e59d5241e05b778629056840544a1debe
michaelandom03 <michaelandom03@gmail.com>
2021-12-15 11:48:42 +0300

    step 1

---
+++ components/ProfileImageComp.jsx
@@ -0,0 +1,11 @@
+function ProfileImageComp({imageSrc,style}) {
+    return (
+        <img
+          src={imageSrc}
+           alt=""
+          className={style}
+        />
+    )
+}
+
+export default ProfileImageComp
---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Feed from '../components/Feed'
+import Sidebar from '../components/Sidebar'
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+      <Sidebar />
+      <Feed />
+      {/* widgets   */}
+      {/* modal   */}
+      </main>    
+    </div>
+  )
+}
